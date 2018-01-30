e74f91b91a2ac1cabf344ecbde37a8ace9655be8
sunil <suniltalukdar82@gmail.com>
2018-01-31 01:06:02 +0530

    after adding login method for customer

---
+++ myBank/src/main/java/com/myBank/app/controller/CustomerController.java
@@ -0,0 +1,33 @@
+package com.myBank.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myBank.app.model.Customer;
+import com.myBank.app.service.LogInService;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@Autowired
+	LogInService logInService;
+	
+	@GetMapping(value = "/logIn",headers = "Accept=application/json,application/xml")
+	public ResponseEntity<Customer> getCustomer(@RequestBody Customer customer){
+		try{
+			Customer customerObj = logInService.logInCustomer(customer);
+			return new ResponseEntity<Customer>(customerObj,HttpStatus.OK);
+		}catch(Exception e){
+			return new ResponseEntity<Customer>(HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+	
+}
---
+++ myBank/src/main/java/com/myBank/app/service/LogInService.java
@@ -0,0 +1,17 @@
+package com.myBank.app.service;
+
+import org.springframework.stereotype.Service;
+
+import com.myBank.app.model.Customer;
+
+@Service
+public class LogInService {
+	
+	public Customer logInCustomer(Customer customer){
+		/*
+		 * needs to be written
+		 */
+		return customer;
+	}
+	
+}
